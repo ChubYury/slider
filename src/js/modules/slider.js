eb3c46dc2b2fe381c00749fb21cd2333570b7f74
ChubYury <yurichubuser@gmail.com>
2024-11-08 09:23:47 +0100

    add slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,3 +1,34 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import 'slick-carousel';
+import $ from 'jquery';
 
-const swiper = new Swiper();
+// import 'slick-carousel';
+// import $ from 'jquery';
+
+const goodsOption = {
+
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        // arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        // arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+}
+
+$('.js-slider').slick(goodsOption);
